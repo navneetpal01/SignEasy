d288bd4c6becd259416369381bcf5b3268a79329
navneet <navneetpalgithub@gmail.com>
2024-06-16 11:16:28 +0530

    Mars

--- app/src/main/java/com/example/signeasy/ui/theme/Color.kt
+++ app/src/main/java/com/example/signeasy/ui/theme/Color.kt
@@ -1,5 +1,4 @@
 package com.example.signeasy.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +8,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val System_White = Color(0xFFF5F5F5)
+val System__Bright_Blue = Color(0xFF407FF7)
+val Smoky_Gray = Color(0xFF7B828A)
+val Dark_Slate_Gray = Color(0xFF39434F)
+val Very_Lite_Gray = Color(0xFFD7D9DC)
+val Dove_Gray = Color(0xFF9CA1A7)
--- app/src/main/java/com/example/signeasy/ui/theme/Type.kt
+++ app/src/main/java/com/example/signeasy/ui/theme/Type.kt
@@ -2,9 +2,26 @@ package com.example.signeasy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.signeasy.R
+
+
+val satoshi_black = FontFamily(Font(R.font.satoshi_black))
+val satoshi_black_italic = FontFamily(Font(R.font.satoshi_black_italic))
+val satoshi_bold = FontFamily(Font(R.font.satoshi_bold))
+val satoshi_bold_italic = FontFamily(Font(R.font.satoshi_bold_italic))
+val satoshi_italic = FontFamily(Font(R.font.satoshi_italic))
+val satoshi_light = FontFamily(Font(R.font.satoshi_light))
+val satoshi_light_italic = FontFamily(Font(R.font.satoshi_light_italic))
+val satoshi_medium = FontFamily(Font(R.font.satoshi_medium))
+val satoshi_medium_italic = FontFamily(Font(R.font.satoshi_medium_italic))
+val satoshi_regular = FontFamily(Font(R.font.satoshi_regular))
+
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
