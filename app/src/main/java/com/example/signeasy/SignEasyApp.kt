92eb7b3476456767a8b4e8c593830bb326225e09
navneet <navneetpalgithub@gmail.com>
2024-06-17 11:46:44 +0530

    Mars

--- app/src/main/java/com/example/signeasy/SignEasyApp.kt
+++ app/src/main/java/com/example/signeasy/SignEasyApp.kt
@@ -1,10 +1,19 @@
 package com.example.signeasy
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
@@ -16,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -31,6 +41,8 @@ import com.example.signeasy.presentation.nvgraph.SignEasyTabs
 import com.example.signeasy.ui.theme.SignEasyTheme
 import com.example.signeasy.ui.theme.Smoky_Gray
 import com.example.signeasy.ui.theme.System_Bright_Blue
+import com.example.signeasy.ui.theme.System_White
+import com.example.signeasy.ui.theme.White
 
 
 @Composable
@@ -42,18 +54,37 @@ fun SignEasyApp() {
         }
         val navController = rememberNavController()
 
-        Scaffold(
-            containerColor = Color.Cyan,
-            bottomBar = {
-                AppBottomBar(navController = navController, tabs = tabs)
-            }
-        ) { paddingValues ->
-            NavGraph(
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+            Scaffold(
                 modifier = Modifier
-                    .padding(paddingValues),
-                navController = navController,
-                startDestination = Route.HomeScreen.route
-            )
+                    .fillMaxSize(),
+                containerColor = System_White,
+                bottomBar = {
+                    AppBottomBar(navController = navController, tabs = tabs)
+                }
+            ) { paddingValues ->
+                NavGraph(
+                    modifier = Modifier
+                        .padding(paddingValues),
+                    navController = navController,
+                    startDestination = Route.HomeScreen.route
+                )
+            }
+            FloatingActionButton(
+                modifier = Modifier
+                    .size(140.dp)
+                    .align(Alignment.BottomCenter)
+                    .padding(all = 40.dp),
+                onClick = {
+                    //TODO
+                },
+                elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
+                shape = CircleShape,
+            ) {
+                Icon(painter = painterResource(id = R.drawable.add), contentDescription = null)
+            }
         }
     }
 }
@@ -71,7 +102,8 @@ fun AppBottomBar(navController: NavHostController, tabs: Array<SignEasyTabs>) {
         NavigationBar(
             modifier = Modifier.windowInsetsBottomHeight(
                 WindowInsets.navigationBars.add(WindowInsets(bottom = 56.dp))
-            )
+            ),
+            containerColor = White
         ) {
             tabs.forEach { tab ->
                 NavigationBarItem(
@@ -89,10 +121,16 @@ fun AppBottomBar(navController: NavHostController, tabs: Array<SignEasyTabs>) {
                         }
                     },
                     icon = {
-                        Icon(painter = painterResource(id = tab.icon), contentDescription = "null")
-                    },
-                    label = {
-                        Text(text = stringResource(id = tab.title))
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Icon(
+                                painter = painterResource(id = tab.icon),
+                                contentDescription = "null"
+                            )
+                            Text(text = stringResource(id = tab.title))
+                        }
                     },
                     alwaysShowLabel = false,
                     colors = NavigationBarItemDefaults.colors(
--- app/src/main/java/com/example/signeasy/ui/theme/Color.kt
+++ app/src/main/java/com/example/signeasy/ui/theme/Color.kt
@@ -10,6 +10,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 
+val White = Color(0xFFFFFFFF)
 val System_White = Color(0xFFF5F5F5)
 val System_Bright_Blue = Color(0xFF407FF7)
 val Smoky_Gray = Color(0xFF7B828A)
