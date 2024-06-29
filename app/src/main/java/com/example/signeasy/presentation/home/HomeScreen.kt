1d80ffa2462d310427044fcf6253194245501697
navneet <navneetpalgithub@gmail.com>
2024-06-29 23:29:34 +0530

    Mars

--- app/src/main/java/com/example/signeasy/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/signeasy/presentation/home/HomeScreen.kt
@@ -1,14 +1,17 @@
 package com.example.signeasy.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,25 +19,51 @@ import androidx.compose.ui.unit.dp
 import com.example.signeasy.ui.theme.White
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         item {
             Card(
                 modifier = Modifier
                     .fillMaxWidth(0.9f)
                     .height(225.dp),
                 colors = CardDefaults.cardColors(contentColor = White, containerColor = White)
-            ){
+            ) {
 
             }
+            Spacer(modifier = Modifier.height(20.dp))
         }
 
-        item {
 
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Card(
+                    modifier = Modifier
+                        .weight(0.45f)
+                        .height(150.dp),
+                    colors = CardDefaults.cardColors(containerColor = White)
+                ) {
+
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Card(
+                    modifier = Modifier
+                        .weight(0.45f)
+                        .height(150.dp),
+                    colors = CardDefaults.cardColors(containerColor = White)
+                ) {
+
+                }
+
+            }
         }
     }
 }
