2664234ec0abd0f8311f687c393828c607361d53
navneet <navneetpalgithub@gmail.com>
2024-06-30 12:17:34 +0530

    Mars

--- app/src/main/java/com/example/signeasy/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/signeasy/presentation/home/HomeScreen.kt
@@ -6,15 +6,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.signeasy.ui.theme.White
+import com.example.signeasy.ui.theme.satoshi_bold
 
 @Composable
 fun HomeScreen() {
@@ -28,9 +36,28 @@ fun HomeScreen() {
                 modifier = Modifier
                     .fillMaxWidth(0.9f)
                     .height(225.dp),
-                colors = CardDefaults.cardColors(contentColor = White, containerColor = White)
+                colors = CardDefaults.cardColors(containerColor = White),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                shape = RoundedCornerShape(10.dp)
             ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Account Usage",
+                        fontFamily = satoshi_bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                }
+                Row {
 
+                }
             }
             Spacer(modifier = Modifier.height(20.dp))
         }
@@ -47,7 +74,9 @@ fun HomeScreen() {
                     modifier = Modifier
                         .weight(0.45f)
                         .height(150.dp),
-                    colors = CardDefaults.cardColors(containerColor = White)
+                    colors = CardDefaults.cardColors(containerColor = White),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                    shape = RoundedCornerShape(10.dp)
                 ) {
 
                 }
@@ -56,7 +85,9 @@ fun HomeScreen() {
                     modifier = Modifier
                         .weight(0.45f)
                         .height(150.dp),
-                    colors = CardDefaults.cardColors(containerColor = White)
+                    colors = CardDefaults.cardColors(containerColor = White),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                    shape = RoundedCornerShape(10.dp)
                 ) {
 
                 }
